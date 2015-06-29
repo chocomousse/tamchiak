01c79b737859c13ef7ba4da0a545fd2285177e26
Chocomousse <tamchiakorbital@gmail.com>
2015-06-29 04:16:24 +0000

    Modeled Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
