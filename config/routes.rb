ab86094fa91b007aef30074f7fa9a817a17eee26
Chocomousse <tamchiakorbital@gmail.com>
2015-06-29 15:41:05 +0000

    fixed the conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   
-
-  get 'csessions/new'
-
   #get 'channels_sessions/new'
   #get 'channels/new'
   root 'sessions#new'
   
   #For channels
   get 'create_channel' => 'channels#new'
-  get 'join_channel' => 'csessions#new'
-  post 'join_channel' => 'csessions#create'
-  delete 'exit_channel' => 'csessions#destroy'
+  get 'join_channel' => 'channels#dummy'
+  #post 'join_channel' => 'csessions#create'
+  #delete 'exit_channel' => 'csessions#destroy'
+  get 'join_or_create' => 'users#choose'
+  get 'new_channel' => 'channels#show'
   
   #For user log in log out
   get 'signup' => 'users#new'
