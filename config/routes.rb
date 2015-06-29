f86b4ae9549253a4cfeb5a5ebee10a72fd985dbf
Chocomousse <tamchiakorbital@gmail.com>
2015-06-29 13:53:45 +0000

    changes to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   
-
   #get 'channels_sessions/new'
   #get 'channels/new'
   root 'sessions#new'
   
   #For channels
   get 'create_channel' => 'channels#new'
-  #get 'join_channel' => 'channels_sessions#new'
-  #post 'join_channel' => 'channels_sessions#create'
-  #delete 'exit_channel' => 'channel_sessions#destroy'
+  get 'join_channel' => 'csessions#new'
+  post 'join_channel' => 'csessions#create'
+  delete 'exit_channel' => 'csessions#destroy'
   
   #For user log in log out
   get 'signup' => 'users#new'
