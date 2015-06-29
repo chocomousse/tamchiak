7935221afef060ae65558ec40ef36007a5f7f387
Chocomousse <tamchiakorbital@gmail.com>
2015-06-29 15:51:47 +0000

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  delete 'logout' => 'session#destroy'
+  delete 'logout' => 'sessions#destroy'
   get 'logout' => 'sessions#new'
   
   #For menu
