628bad0866fd908c8501bf6549ce7887f6e61cc2
Chocomousse <tamchiakorbital@gmail.com>
2015-07-06 10:39:20 +0000

    added the form-control classes

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -5,14 +5,6 @@ class ChannelTest < ActiveSupport::TestCase
     @channel = Channel.new(cname: "channelOne", menu: "Ameens")
   end 
   
-  test "menu type only accepts Ameens or Macs" do
-    valid_channelNames = %w[ameens macs ameen]
-    valid_channelNames.each do |valid_channelName|
-      @channel.channelName = valid_channelName
-      assert @channel.valid?, "#{valid_channelName.inspect} should be valid"
-    end
-  end 
-  
   test "channel name has been taken" do
     duplicate_channel = @channel.dup
     @channel.save
