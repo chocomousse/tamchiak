a750947f689c1f770fec54789c6144c086c4f6e4
Chocomousse <tamchiakorbital@gmail.com>
2015-07-14 06:41:49 +0000

    updated the models of channel and ameens_menu

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,8 @@
 class Channel < ActiveRecord::Base
+  #belongs_to :users
+  has_many :users
+  has_many :ameens_menus, dependent: :destroy
+  validates :cname, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :menu, presence: true
+
 end
