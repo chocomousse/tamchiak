488111a4658b7f80c7d740bf55dd2c9943ebca22
Chocomousse <tamchiakorbital@gmail.com>
2015-07-14 15:43:50 +0000

    Removed the redone model and controller for Ameens Menu

--- app/models/ameen.rb
+++
@@ -1,7 +0,0 @@
-class Ameen < ActiveRecord::Base
-  validates :category, presence: true
-  validates :subcat, presence: true
-  validates :item_code, presence: true
-  validates :name, presence: true
-  validates :price, presence: true
-end
