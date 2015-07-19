602d690ac30756ed9fd20c6df6b1a4045d345781
Chocomousse <tamchiakorbital@gmail.com>
2015-07-19 16:11:23 +0000

    Removed redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_filter :logged_in_user
 
   # Returns the current order
   def current_order
