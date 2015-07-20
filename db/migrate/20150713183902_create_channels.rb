76765c51b6ed51881f340dcd8e765da4362d6c2f
Chocomousse <tamchiakorbital@gmail.com>
2015-07-20 05:43:52 +0000

    Removed index on user_id and created_at from channels

--- db/migrate/20150713183902_create_channels.rb
+++ db/migrate/20150713183902_create_channels.rb
@@ -6,6 +6,5 @@ class CreateChannels < ActiveRecord::Migration
       t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
-    add_index :users, [:user_id, :created_at]
   end
 end
