093f6ac3b73bb9f98700ef1740dc00bc635187d1
Chocomousse <tamchiakorbital@gmail.com>
2015-07-20 06:18:44 +0000

    Discarded attempts to fix heroku migrate bug

--- db/migrate/20150720055942_add_index_to_channels_user_id_and_created_at.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToChannelsUserIdAndCreatedAt < ActiveRecord::Migration
-  def change
-    add_index :channels, [:user_id, :created_at]
-  end
-end
--- db/migrate/20150720060547_add_user_id_and_created_at_to_channels.rb
+++
@@ -1,4 +0,0 @@
-class AddUserIdAndCreatedAtToChannels < ActiveRecord::Migration
-  def change
-  end
-end
