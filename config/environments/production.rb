bc5c0d2c7478630f516fd488cdaf0c9593b07cf4
Chocomousse <tamchiakorbital@gmail.com>
2015-07-20 13:00:38 +0000

    activation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,15 +80,17 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.perform_deliveries = true
+  host = 'tamchiak.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain               'gmail.com',
+    :user_name            'tamchiakorbital@gmail.com',
+    :password             'orbital19',
     :enable_starttls_auto => true
   }  
+
 end
