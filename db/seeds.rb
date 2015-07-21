d485500d6ae1e49220e028a5b8c21019d967cb10
Chocomousse <tamchiakorbital@gmail.com>
2015-07-21 04:33:45 +0000

    completed thai kitchen menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-MenuItem.delete_all
+#********************************************************************************Thai Kitchen************************************************************************************
+#Mee, Beehoon, Kwayteow, Maggie
 MenuItem.create! id: 1, name: "Tomyam - Beef", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
 MenuItem.create! id: 2, name: "Tomyam - Chicken", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
 MenuItem.create! id: 3, name: "Tomyam - Seafood", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
+MenuItem.create! id: 4, name: "Bandung, Soup", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T31", price: 4.00, active: true
+MenuItem.create! id: 5, name: "Hong Kong (white sauce)", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T32", price: 4.00, active: true
+MenuItem.create! id: 6, name: "Haillam (black sauce)", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T33", price: 4.00, active: true
+MenuItem.create! id: 7, name: "Thai Style Fried with Mixture Chicken Prawn and Cuttlefish", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T34", price: 4.50, active: true
+MenuItem.create! id: 8, name: "Sambal Style Fried (serve with chicken)", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T35", price: 4.00, active: true
+MenuItem.create! id: 9, name: "Chinese Style Friend (serve with chicken)", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T36", price: 4.00, active: true
+MenuItem.create! id: 10, name: "Fried with Beef", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T37", price: 4.00, active: true
+MenuItem.create! id: 11, name: "Fried with Seafood (cuttle fish and prawn)", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T38", price: 4.00, active: true
+MenuItem.create! id: 12, name: "Pataya - Chicken", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T39", price: 5.00, active: true
+MenuItem.create! id: 13, name: "Pataya - Beef", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T39", price: 5.00, active: true
+MenuItem.create! id: 14, name: "Pataya - Seafood", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T39", price: 5.00, active: true
+MenuItem.create! id: 15, name: "Fried with cockles", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T40", price: 4.00, active: true
+
+#Steam Rice (Chicken, Beef, Slice Fish, Cuttlefish, Prawn)
+MenuItem.create! id: 16, name: "Hot & Spicy with Red Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T41", price: 4.50, active: true
+MenuItem.create! id: 17, name: "Black Oyster Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T42", price: 4.50, active: true
+MenuItem.create! id: 18, name: "Sweet & Sour", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T43", price: 4.50, active: true
+MenuItem.create! id: 19, name: "Black Pepper", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T44", price: 4.80, active: true
+MenuItem.create! id: 20, name: "Ginger Brown Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T45", price: 4.80, active: true
+MenuItem.create! id: 21, name: "Mui Fun (thick clear sauce)", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T46", price: 4.00, active: true
+
+#Fried Rice
+MenuItem.create! id: 22, name: "Chinese Style Serve with Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T47", price: 4.00, active: true
+MenuItem.create! id: 23, name: "Thai Style Serve with Micture Chicken Prawn, Cuttle Fish", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T48", price: 4.50, active: true
+MenuItem.create! id: 24, name: "Ikan Billis & Egg", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T49", price: 4.00, active: true
+MenuItem.create! id: 25, name: "Tomato & Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T50", price: 4.00, active: true
+MenuItem.create! id: 26, name: "Fried Chicken with Chilli Sauce", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T51", price: 4.00, active: true
+MenuItem.create! id: 27, name: "Salted Fish", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T52", price: 4.00, active: true
+MenuItem.create! id: 28, name: "Kampong Style", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T53", price: 4.50, active: true
+MenuItem.create! id: 29, name: "Cockles", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T54", price: 4.00, active: true
+MenuItem.create! id: 30, name: "Pataya - Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T55", price: 5.00, active: true
+MenuItem.create! id: 31, name: "Pataya - Beef", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T55", price: 5.00, active: true
+MenuItem.create! id: 32, name: "Pataya - Seafood", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T55", price: 5.00, active: true
+MenuItem.create! id: 33, name: "Chinese or Thai Style with Red Chilli Beef", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T56", price: 5.00, active: true
+MenuItem.create! id: 34, name: "Chinese Style with Hot & Spicy Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T57", price: 5.00, active: true
+MenuItem.create! id: 35, name: "Chinese Style with Hot & Spicy Seafood", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T57", price: 5.00, active: true
+MenuItem.create! id: 36, name: "Thai Style with Hot & Spicy Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T58", price: 5.00, active: true
+MenuItem.create! id: 37, name: "Thai Style with Hot & Spicy Seafood", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T58", price: 5.00, active: true
+MenuItem.create! id: 38, name: "Chinese Style with Black Oyster Beef", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T59", price: 5.00, active: true
+MenuItem.create! id: 39, name: "Chinese Style with Black Oyster Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T59", price: 5.00, active: true
+MenuItem.create! id: 40, name: "Button Mushroom & Egg", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T60", price: 4.00, active: true
+MenuItem.create! id: 41, name: "Sambal Mushroom & Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T61", price: 5.00, active: true
+MenuItem.create! id: 42, name: "3 Taste (Sweet, sour, chilli) with Chicken & Seafood", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T62", price: 6.00, active: true
+MenuItem.create! id: 43, name: "Sambal with Fried Sambal Chickem", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T63", price: 5.00, active: true
+MenuItem.create! id: 44, name: "Black Pepper Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T64", price: 5.00, active: true
+MenuItem.create! id: 45, name: "Black Pepper Beef", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T64", price: 5.00, active: true
+MenuItem.create! id: 46, name: "Pineapple (serve with chicken and seafood)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T65", price: 5.00, active: true
+MenuItem.create! id: 47, name: "Chinese Style with Sweet & Sour (chicken, slice fish)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T66", price: 5.00, active: true
+MenuItem.create! id: 48, name: "Chinese Style with Ginger Brown Sauce (Chicken, beef)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T67", price: 5.00, active: true
+MenuItem.create! id: 49, name: "Thai Style with Crispy Ginger Yellow Chicken & Egg", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T68", price: 6.00, active: true
+MenuItem.create! id: 50, name: "Bush (yellow fried rice with egg & hot & spicy chicken)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T69", price: 6.00, active: true
+MenuItem.create! id: 51, name: "Obama (yellow fried rice with egg & hot & spicy beef)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T70", price: 6.00, active: true
+MenuItem.create! id: 52, name: "Thai Style with Chicken Egg Sambal", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T71", price: 5.00, active: true
+MenuItem.create! id: 53, name: "Chinese Style with Chicken Egg Sambal", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T71", price: 5.00, active: true
 
 ChannelStatus.delete_all
 ChannelStatus.create! id: 1, name: "Open"
