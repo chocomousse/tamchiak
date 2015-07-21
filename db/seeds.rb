330d0ac62d90e9740040b73d39ace3e63881e48f
Chocomousse <tamchiakorbital@gmail.com>
2015-07-21 05:23:42 +0000

    halfway through Indian Kitchen menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
+MenuItem.delete_all
 #********************************************************************************Thai Kitchen************************************************************************************
-#Mee, Beehoon, Kwayteow, Maggie
+# Mee, Beehoon, Kwayteow, Maggie
 MenuItem.create! id: 1, name: "Tomyam - Beef", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
 MenuItem.create! id: 2, name: "Tomyam - Chicken", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
 MenuItem.create! id: 3, name: "Tomyam - Seafood", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T30", price: 6.50, active: true
@@ -16,7 +17,7 @@ MenuItem.create! id: 13, name: "Pataya - Beef", cat: "Thai Kitchen", subcat: "Me
 MenuItem.create! id: 14, name: "Pataya - Seafood", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T39", price: 5.00, active: true
 MenuItem.create! id: 15, name: "Fried with cockles", cat: "Thai Kitchen", subcat: "Mee, Beehoon, Kwayteow, Maggie", item_code: "T40", price: 4.00, active: true
 
-#Steam Rice (Chicken, Beef, Slice Fish, Cuttlefish, Prawn)
+# Steam Rice (Chicken, Beef, Slice Fish, Cuttlefish, Prawn)
 MenuItem.create! id: 16, name: "Hot & Spicy with Red Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T41", price: 4.50, active: true
 MenuItem.create! id: 17, name: "Black Oyster Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T42", price: 4.50, active: true
 MenuItem.create! id: 18, name: "Sweet & Sour", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T43", price: 4.50, active: true
@@ -24,7 +25,7 @@ MenuItem.create! id: 19, name: "Black Pepper", cat: "Thai Kitchen", subcat: "Ste
 MenuItem.create! id: 20, name: "Ginger Brown Sauce", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T45", price: 4.80, active: true
 MenuItem.create! id: 21, name: "Mui Fun (thick clear sauce)", cat: "Thai Kitchen", subcat: "Steam Rice", item_code: "T46", price: 4.00, active: true
 
-#Fried Rice
+# Fried Rice
 MenuItem.create! id: 22, name: "Chinese Style Serve with Chicken", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T47", price: 4.00, active: true
 MenuItem.create! id: 23, name: "Thai Style Serve with Micture Chicken Prawn, Cuttle Fish", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T48", price: 4.50, active: true
 MenuItem.create! id: 24, name: "Ikan Billis & Egg", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T49", price: 4.00, active: true
@@ -57,6 +58,103 @@ MenuItem.create! id: 50, name: "Bush (yellow fried rice with egg & hot & spicy c
 MenuItem.create! id: 51, name: "Obama (yellow fried rice with egg & hot & spicy beef)", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T70", price: 6.00, active: true
 MenuItem.create! id: 52, name: "Thai Style with Chicken Egg Sambal", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T71", price: 5.00, active: true
 MenuItem.create! id: 53, name: "Chinese Style with Chicken Egg Sambal", cat: "Thai Kitchen", subcat: "Fried Rice", item_code: "T71", price: 5.00, active: true
+
+
+#*******************************************************************************Indian Kitchen*************************************************************************************
+# Tandoori => Need Different Prices
+MenuItem.create! id: 54, name: "Tandoori Chicken", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N01", price: , active: true
+MenuItem.create! id: 55, name: "Mixed Chicken Plater", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N02", price: , active: true
+MenuItem.create! id: 56, name: "Chicken Tikka", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N03", price: , active: true
+MenuItem.create! id: 57, name: "Chicken Hariyall", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N04", price: , active: true
+MenuItem.create! id: 58, name: "Chicken Reshmi Kebab", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N05", price: , active: true
+MenuItem.create! id: 59, name: "Chicken Iranian Kebab", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N06", price: , active: true
+MenuItem.create! id: 60, name: "Seekh Kebab", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N07", price: , active: true
+MenuItem.create! id: 61, name: "Panner Tikka", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N08", price: , active: true
+MenuItem.create! id: 62, name: "Fish Tikka", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N09", price: , active: true
+MenuItem.create! id: 63, name: "Fish Hariyall", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N10", price: , active: true
+MenuItem.create! id: 64, name: "Fish Reshmi", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N11", price: , active: true
+MenuItem.create! id: 65, name: "Mix Fish Plater", cat: "Indian Kitchen", subcat: "Tandoori", item_code: "N12", price: , active: true
+
+# Breads
+MenuItem.create! id: 66, name: "Plain Naan", cat: "Indian Kitchen", subcat: "Breads", item_code: "N13", price: 1.30, active: true
+MenuItem.create! id: 67, name: "Butter Naan", cat: "Indian Kitchen", subcat: "Breads", item_code: "N14", price: 1.50, active: true
+MenuItem.create! id: 68, name: "Garlic Naan", cat: "Indian Kitchen", subcat: "Breads", item_code: "N15", price: 2.00, active: true
+MenuItem.create! id: 69, name: "Kashmiri Naan (top with dry fruits)", cat: "Indian Kitchen", subcat: "Breads", item_code: "N16", price: 4.00, active: true
+MenuItem.create! id: 70, name: "Cheese Naan", cat: "Indian Kitchen", subcat: "Breads", item_code: "N17", price: 3.00, active: true
+MenuItem.create! id: 71, name: "Kheema Naan", cat: "Indian Kitchen", subcat: "Breads", item_code: "N18", price: 4.00, active: true
+MenuItem.create! id: 72, name: "Aloo Pratha", cat: "Indian Kitchen", subcat: "Breads", item_code: "N19", price: 2.50, active: true
+MenuItem.create! id: 73, name: "Poodina Pratha", cat: "Indian Kitchen", subcat: "Breads", item_code: "N20", price: 2.00, active: true
+MenuItem.create! id: 74, name: "Garlic & Onion Kulcha", cat: "Indian Kitchen", subcat: "Breads", item_code: "N21", price: 3.00, active: true
+MenuItem.create! id: 75, name: "Tandoori Roti", cat: "Indian Kitchen", subcat: "Breads", item_code: "N22", price: 1.30, active: true
+MenuItem.create! id: 76, name: "Paneer Kulcha", cat: "Indian Kitchen", subcat: "Breads", item_code: "N23", price: 3.00, active: true
+
+# Chicken
+MenuItem.create! id: 77, name: "Chicken Korma", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N24", price: 6.00, active: true
+MenuItem.create! id: 78, name: "Chicken Spinach", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N25", price: 7.00, active: true
+MenuItem.create! id: 79, name: "Chicken Masala", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N26", price: 6.00, active: true
+MenuItem.create! id: 80, name: "Chicken Vartha", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N27", price: 6.00, active: true
+MenuItem.create! id: 81, name: "Chicken Jalfrazzi", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N28", price: 8.00, active: true
+MenuItem.create! id: 82, name: "Chicken Muglai", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N29", price: 7.00, active: true
+MenuItem.create! id: 83, name: "Butter Chicken", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N30", price: 7.00, active: true
+MenuItem.create! id: 83, name: "Chicken Tikka Masala", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N31", price: 7.00, active: true
+MenuItem.create! id: 83, name: "Kadai Chicken", cat: "Indian Kitchen", subcat: "Chicken", item_code: "N32", price: 8.00, active: true
+
+# Rice
+MenuItem.create! id: 84, name: "Chicken Briyani", cat: "Indian Kitchen", subcat: "Rice", item_code: "N33", price: 6.00, active: true
+MenuItem.create! id: 85, name: "Mutton Briyani", cat: "Indian Kitchen", subcat: "Rice", item_code: "N34", price: 6.00, active: true
+MenuItem.create! id: 86, name: "Prawn Briyani", cat: "Indian Kitchen", subcat: "Rice", item_code: "N35", price: 7.00, active: true
+MenuItem.create! id: 87, name: "Vegetable Briyani", cat: "Indian Kitchen", subcat: "Rice", item_code: "N36", price: 5.00, active: true
+MenuItem.create! id: 88, name: "Kashmiri Pulao", cat: "Indian Kitchen", subcat: "Rice", item_code: "N37", price: 4.00, active: true
+MenuItem.create! id: 89, name: "Jeera Rice", cat: "Indian Kitchen", subcat: "Rice", item_code: "N38", price: 3.00, active: true
+MenuItem.create! id: 90, name: "Plain White Rice", cat: "Indian Kitchen", subcat: "Rice", item_code: "N39", price: 1.00, active: true
+
+# Mutton
+MenuItem.create! id: 91, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N40", price: , active: true
+MenuItem.create! id: 92, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N41", price: , active: true
+MenuItem.create! id: 93, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N42", price: , active: true
+MenuItem.create! id: 94, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N43", price: , active: true
+MenuItem.create! id: 95, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N44", price: , active: true
+MenuItem.create! id: 96, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N45", price: , active: true
+MenuItem.create! id: 97, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N46", price: , active: true
+MenuItem.create! id: 98, name: "", cat: "Indian Kitchen", subcat: "Mutton", item_code: "N47", price: , active: true
+
+# Seafood
+MenuItem.create! id: 99, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N48", price: , active: true
+MenuItem.create! id: 100, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N49", price: , active: true
+MenuItem.create! id: 101, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N50", price: , active: true
+MenuItem.create! id: 102, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N51", price: , active: true
+MenuItem.create! id: 103, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N52", price: , active: true
+MenuItem.create! id: 104, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N53", price: , active: true
+MenuItem.create! id: 105, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N54", price: , active: true
+MenuItem.create! id: 106, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N55", price: , active: true
+MenuItem.create! id: 107, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N56", price: , active: true
+MenuItem.create! id: 108, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N57", price: , active: true
+MenuItem.create! id: 109, name: "", cat: "Indian Kitchen", subcat: "Seafood", item_code: "N58", price: , active: true
+
+# Vegetables
+MenuItem.create! id: 110, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N59", price: , active: true
+MenuItem.create! id: 111, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N60", price: , active: true
+MenuItem.create! id: 112, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N61", price: , active: true
+MenuItem.create! id: 113, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N62", price: , active: true
+MenuItem.create! id: 114, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N63", price: , active: true
+MenuItem.create! id: 115, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N64", price: , active: true
+MenuItem.create! id: 116, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N65", price: , active: true
+MenuItem.create! id: 117, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N66", price: , active: true
+MenuItem.create! id: 118, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N67", price: , active: true
+MenuItem.create! id: 119, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N68", price: , active: true
+MenuItem.create! id: 120, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N69", price: , active: true
+MenuItem.create! id: 121, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N70", price: , active: true
+MenuItem.create! id: 122, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N71", price: , active: true
+MenuItem.create! id: 123, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N72", price: , active: true
+MenuItem.create! id: 124, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N73", price: , active: true
+MenuItem.create! id: 125, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N74", price: , active: true
+MenuItem.create! id: 126, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N75", price: , active: true
+MenuItem.create! id: 127, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N76", price: , active: true
+MenuItem.create! id: 128, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N77", price: , active: true
+MenuItem.create! id: 129, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N78", price: , active: true
+MenuItem.create! id: 130, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N79", price: , active: true
+MenuItem.create! id: 131, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N80", price: , active: true
+MenuItem.create! id: 132, name: "", cat: "Indian Kitchen", subcat: "Vegetables", item_code: "N81", price: , active: true
 
 ChannelStatus.delete_all
 ChannelStatus.create! id: 1, name: "Open"
