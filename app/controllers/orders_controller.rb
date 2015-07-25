ecf871fc573d4a47282306d58a5c854bcb33958f
Chocomousse <tamchiakorbital@gmail.com>
2015-07-25 03:04:55 +0000

    Attempt to fix grouping error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,27 +2,6 @@ class OrdersController < ApplicationController
   before_action :logged_in_user
   before_action :in_a_channel
 
-  def new
-    @menu_items = MenuItem.group(:cat)
-    @order = Order.new
-  end
-
-  # Returns the current order
-  def current_order
-    @current_order ||= Order.find_by(id: user_id).all
-  end
-  
-  def collate
-    @channel = current_channel
-    @relevant_orders ||= Order.where(channel_id: current_channel.id).group(:meal)
-    @orders_in_channel ||= Order.where(channel_id: current_channel.id)
-    @specific_orders ||= Order.where(channel_id: current_channel.id).group(:meal, :subcat)
-  end 
-
-  def show
-    @order = Order.all
-  end
-
   def create 
     @menu_items = MenuItem.all
     @order = current_channel.orders.build(order_params)
@@ -36,6 +15,32 @@ class OrdersController < ApplicationController
       render 'new'
     end 
   end 
+  
+  def new
+    #@menu_items = MenuItem.group(:cat)
+    @menu_items = MenuItem.select("DISTINCT ON (menu_items.cat) *").group("menu_items.*")
+    @order = Order.new
+  end
+
+  # Returns the current order
+  def current_order
+    @current_order ||= Order.find_by(id: user_id).all
+  end
+
+  def collate
+    @channel = current_channel
+    #@relevant_orders ||= Order.where(channel_id: current_channel.id).group(:meal)
+    #@orders_in_channel ||= Order.where(channel_id: current_channel.id)
+    #@specific_orders ||= Order.where(channel_id: current_channel.id).group(:meal, :subcat)
+    @relevant_orders ||= Order.where(channel_id: current_channel.id).group(:meal)
+    @orders_in_channel ||= Order.where(channel_id: current_channel.id)
+    @specific_orders ||= Order.where(channel_id: current_channel.id).group(:meal, :subcat)
+  
+  end 
+
+  def show
+    @order = Order.all
+  end
 
   def destroy
     @order = current_channel.orders.find(params[:id])
