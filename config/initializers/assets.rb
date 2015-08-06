124b0809d9858e278d13c702ecbe5b61b14a1daf
Chocomousse <tamchiakorbital@gmail.com>
2015-08-06 03:35:11 +0000

    added paths to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( custom.css )
 Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( bootsnip.js )
