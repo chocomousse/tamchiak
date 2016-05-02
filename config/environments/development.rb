df423ff1753b7e00f5d19e62a8d8fe680abf3dcd
Victor Hazali <victorhazali@gmail.com>
2016-05-02 16:39:11 +0800

    Removed port number

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   #config.action_mailer.default_options = {from: 'tamchiakorbital@gmail.com'}
   #host = 'http://tamchiak-111184.nitrousapp.com'
   host = 'http://tamchiak.tk'
-  config.action_mailer.default_url_options = { host: host, port: 3000 }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
